8382c00bdbd62f1aa18e53018260107d4f6b0dc5
Chris Hartley <chartley.it@gmail.com>
2021-05-14 17:02:02 -0400

    use gofiber to create a basic listener

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/chartleyit/drinkdex
 
 go 1.16
+
+require github.com/gofiber/fiber/v2 v2.9.0
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import "github.com/gofiber/fiber/v2"
+
+const welcome = "Hello, I'm DrinkDex"
 
 func main() {
-	fmt.Println("Hello I'm DrinkDex")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString(welcome)
+	})
+
+	app.Listen(":18150")
 }
